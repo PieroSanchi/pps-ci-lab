84316e231e7476a3ca4717a15ceb65da683cfd13
Piero Sanchi - piero.sanchi@studio.unibo.it <piero.sanchi@studio.unibo.it>
2021-04-26 12:31:55 +0200

    changed indentation in ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
     java
+    application
 }
 
 repositories {
+    mavenCentral()
 }
 
 dependencies {
---
+++ src/main/java/Jojo.java
@@ -0,0 +1,5 @@
+class Jojo {
+    public static void main(String[] args) {
+        System.out.println("Hello Jojo!");
+    }
+}
